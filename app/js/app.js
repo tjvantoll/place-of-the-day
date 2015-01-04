e196a8136f7f272491d70f06b8ec28b7e5f1938a
TJ VanToll <tj.vantoll@gmail.com>
2015-01-04 08:32:52 -0500

    Adding photo functionality

--- app/js/app.js
+++ app/js/app.js
@@ -10,21 +10,49 @@
 		}
 	});
 
-	window.detailsShow = function( event ) {
-		var view = event.view;
-		view.element.find( "[data-role='view-title']" ).text( "Loading..." );
-		el.data( "Places" ).getById( view.params.id ).then(function( data ) {
-			var place = data.result,
-				template = kendo.template( $( "#place-template" ).html() );
+	function loadPictures( place ) {
+		console.log( place );
 
-			$( "#place" ).html( template( place ) );
-			view.element.find( "[data-role='view-title']" ).text( place.Name );
+		$.get( "http://www.panoramio.com/map/get_panoramas.php",
+			{
+				set: "public",
+				from: 0,
+				to: 5,
+				miny: place.Latitude - 5,
+				maxy: place.Latitude + 5,
+				minx: place.Longitude - 5,
+				maxx: place.Longitude + 5,
+				size: "medium"
+			})
+			.then(function( data ) {
+				var photos = $( "#photos" );
+				data.photos.forEach(function( photo ) {
+					photos.append( "<img src='" +
+						photo.photo_file_url +
+						"'>" );
+			})
 		});
 	}
 
-	window.detailsHide = function( event ) {
-		$( "#place" ).empty();
-	}
+	window.detailsView = {
+		show: function ( event ) {
+			var view = event.view;
+			view.element.find( "[data-role='view-title']" ).text( "Loading..." );
+			el.data( "Places" ).getById( view.params.id )
+				.then(function( data ) {
+					var place = data.result,
+						template = kendo.template( $( "#place-template" ).html() );
+
+					$( "#place" ).html( template( place ) );
+					view.element.find( "[data-role='view-title']" ).text( place.Name );
+
+					loadPictures( place );
+				});
+		},
+		hide: function() {
+			$( "#place" ).empty();
+		}
+	};
 
 	document.addEventListener( "deviceready", function () {  
 		navigator.splashscreen.hide();
