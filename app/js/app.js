9b3e7b16d1bf6fdca4d816cbf65419aef73faff3
TJ VanToll <tj.vantoll@gmail.com>
2015-01-12 09:14:21 -0500

    Cleaner formatting

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,20 @@
-require([ "everlive", "push", "views/list", "views/details" ], function( el, push, list, details ) {
-	"use strict";
+require([
+	"everlive",
+	"push",
+	"views/list",
+	"views/details" ],
+	function( el, push, list, details ) {
+		"use strict";
 
-	document.addEventListener( "deviceready", function () {  
-		navigator.splashscreen.hide();
+		document.addEventListener( "deviceready", function () {  
+			navigator.splashscreen.hide();
 
-		var app = new kendo.mobile.Application( document.body, {
-			skin: "flat",
-			init: function() {
-				kendo.UserEvents.defaultThreshold( 20 );
-			}
+			var app = new kendo.mobile.Application( document.body, {
+				skin: "flat",
+				init: function() {
+					kendo.UserEvents.defaultThreshold( 20 );
+				}
+			});
 		});
-	});
-});
+	}
+);
