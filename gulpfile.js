37a92f3e2b39a9d0cc2cabe44d48ad2b11117791
TJ VanToll <tj.vantoll@gmail.com>
2015-01-21 14:53:35 -0500

    Adding CSS linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require( "gulp" ),
-	jshint = require( "gulp-jshint" );
+	jshint = require( "gulp-jshint" ),
+	csslint = require( "gulp-csslint" );
 
-gulp.task( "default", function() {
-
-});
-
-gulp.task( "lint", function() {
+gulp.task( "jshint", function() {
 	return gulp.src( "app/js/**/*.js" )
 		.pipe( jshint() )
-		.pipe( jshint.reporter( "default" ) );
+		.pipe( jshint.reporter() );
 });
+
+gulp.task( "csslint", function() {
+	gulp.src( "app/css/*.css" )
+		.pipe( csslint() )
+		.pipe( csslint.reporter() );
+});
+
+gulp.task( "default", [ "jshint", "csslint" ]);
