f83e0a6860b26ad9ad4ccd78aa32a17b7c2bf80b
TJ VanToll <tj.vantoll@gmail.com>
2015-02-13 10:59:14 -0500

    Work around the double-load iOS bug for now

--- native/app/app/views/details.js
+++ native/app/app/views/details.js
@@ -1,14 +1,23 @@
+var loadedMap;
+
 exports.load = function( args ) {
 	var place = args.context,
 		page = args.object;
 
 	page.ios.title = place.Name + ", " + place.Country;
+	loadedMap = false;
 };
 
 exports.loadMap = function( args ) {
+	if ( loadedMap ) {
+		return;
+	}
+
 	var iosView = args.object.ios,
 		map = MKMapView.alloc().initWithFrame(
-			CGRectMake( 0, 0, UIScreen.mainScreen().bounds.size.width, 200 ) );
+			// CGRectMake( 0, 0, UIScreen.mainScreen().bounds.size.width, 200 ) );
+			UIScreen.mainScreen().bounds );
 
 	iosView.addSubview( map );
+	loadedMap = true;
 }
