190e3cdc7857c42ddefdbae662695039c6faf3e3
TJ VanToll <tj.vantoll@gmail.com>
2015-02-13 09:30:33 -0500

    Adding the map to the StackPanel so that it actually works

--- native/app/app/views/details.js
+++ native/app/app/views/details.js
@@ -3,8 +3,11 @@ exports.load = function( args ) {
 		page = args.object;
 
 	page.ios.title = place.Name + ", " + place.Country;
-
-	var iosView = args.object.ios._page._view;
-	var map = MKMapView.alloc().initWithFrame( CGRectMake( 0, 0, 1000, 1000 ) );
-	iosView.addSubview( map );
 };
+
+exports.loadMap = function( args ) {
+	var iosView = args.object.ios,
+		map = MKMapView.alloc().initWithFrame( CGRectMake( 0, 0, 1000, 1000 ) );
+
+	iosView.addSubview( map );
+}
