b1a43b89e6b41242f5f00aac96cc3d5030ccfe41
TJ VanToll <tj.vantoll@gmail.com>
2015-02-15 16:19:43 -0500

    Tweaking the display a bit

--- native/app/app/views/details.js
+++ native/app/app/views/details.js
@@ -16,8 +16,8 @@ exports.loadMap = function( args ) {
 
 	var iosView = args.object.ios,
 		map = MKMapView.alloc().initWithFrame(
-			 // CGRectMake( 0, 0, UIScreen.mainScreen().bounds.size.width, 200 ) ),
-			 UIScreen.mainScreen().bounds ),
+			CGRectMake( 0, 0, UIScreen.mainScreen().bounds.size.width, 200 ) ),
+			// UIScreen.mainScreen().bounds ),
 		location = CLLocationCoordinate2DMake( place.Latitude, place.Longitude ),
 		span = new MKCoordinateSpan({ latitudeDelta: 0.3, longitudeDelta: 0.3 }),
 		region = new MKCoordinateRegion({ center: location, span: span });
